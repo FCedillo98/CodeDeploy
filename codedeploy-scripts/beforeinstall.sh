620211fefe0ddfcf9dbd15f37824dbe5322f0035
Francisco Cedillo Serrato <118803632+FCedillo98@users.noreply.github.com>
2024-02-13 18:40:17 +0100

    Update beforeinstall.sh

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /opt/codedeploy-agent/deployment-root/deployment-instructions/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+
 chmod +x gradlew
+
 # Compilamos la aplicación con Gradle para obtener un fichero en formato WAR
 ./gradlew war
