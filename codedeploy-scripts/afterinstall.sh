a4739e1d2d8ae16b491dd0dcdc8c0c048fadf676
Francisco Cedillo Serrato <118803632+FCedillo98@users.noreply.github.com>
2024-02-13 19:45:10 +0100

    Update afterinstall.sh

--- codedeploy-scripts/afterinstall.sh
+++ codedeploy-scripts/afterinstall.sh
@@ -5,5 +5,5 @@ cd /opt/tomcat/webapps
 app=holamundo
 version="-0.0.1-plain"
 
-mv `$app$version.war` `$app.war`
-mv `$app$version` `$app`
+mv "$app$version.war" "$app.war"
+mv "$app$version" "$app"
