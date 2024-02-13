d86900328a0fdb626eab0c7cc61977522e341a62
Francisco Cedillo Serrato <118803632+FCedillo98@users.noreply.github.com>
2024-02-13 19:08:18 +0100

    Delete beforeinstall.sh

--- beforeinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-
-chmod +x gradlew
-
-# Compilamos la aplicación con Gradle para obtener un fichero en formato WAR
-./gradlew war
